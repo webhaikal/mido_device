c693f75073276ae44d1b506e84b98391ecce6cd8
Haikal Izzuddin <haikalizz@prototypestd.ml>
2017-08-28 20:33:54 +0800

    mido: rework init for oreo
    Change-Id: I24daef5d2243d2dd4671994450aae35d49fe89a9

--- init/Android.mk
+++ init/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 LOCAL_PATH := $(call my-dir)
 
@@ -21,6 +20,9 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_MODULE := libinit_mido
 LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_mido.cpp
+
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
